575a82cde5367e7676cef52eb44fab0712d3ff59
Francisco Picado <franciscopicado19@gmail.com>
2024-01-23 20:09:54 -0600

    🔧 add configuration alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//Usar font-awesone
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/src/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+    }
+  }
 })
