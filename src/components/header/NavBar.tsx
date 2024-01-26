dcfff7f376c4986c2176ffb8aa2f4052f0226623
Lennwmedia <lennwmedia@gmail.com>
2024-01-26 14:25:39 -0600

    otherCommit

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,60 +1,78 @@
-
-import '@/assets/scss/components/header/Navbar.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "@/assets/scss/components/header/Navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NavBar() {
   const user = {
-    name: 'Lener',
-    age: 20
-  }
+    name: "Lener",
+    age: 20,
+  };
 
   const handleOpenMenuEdit = () => {
-    const customEvent = new CustomEvent('openMenuEdit', {detail: user});
+    const customEvent = new CustomEvent("openMenuEdit", { detail: user });
     window.dispatchEvent(customEvent);
-  }
+  };
 
   return (
     <>
       <header>
-        <nav className='nav-bar'>
+        <nav className="nav-bar">
           <div>
-            <button onClick={handleOpenMenuEdit}><FontAwesomeIcon icon={['fas', 'bars']} /> Photo Editor</button>
+            <button onClick={handleOpenMenuEdit}>
+              <FontAwesomeIcon icon={["fas", "bars"]} /> Photo Editor
+            </button>
           </div>
-          
-          <div>
-            <button>Open <FontAwesomeIcon icon={['fas', 'chevron-down']} /></button>
-            <div>
-              <h4>Nueva imagen</h4>
-              <button>Ordenador</button>
-              <button>PhotoEditor</button>
-              <button>Google Drive</button>
-              <button>Google Fotos</button>
+          <div className="items-center">
+            <div className="dropdown-father-open">
+              <button>
+                Open <FontAwesomeIcon icon={["fas", "chevron-down"]} />
+              </button>
+              <div className="dropdown-son-open">
+                <h4>Nueva imagen</h4>
+                <button>Ordenador</button>
+                <button>PhotoEditor</button>
+                <button>Google Drive</button>
+                <button>Google Fotos</button>
+              </div>
+              <div className="dropdown-son-open">
+                <h4>Proyeccto existente</h4>
+                <button>Ordenador</button>
+                <button>PhotoEditor</button>
+              </div>
             </div>
-            <h4>Proyeccto existente</h4>
+
+            <div className="dropdown-father-save">
+              <button>
+                Save <FontAwesomeIcon icon={["fas", "chevron-down"]} />
+              </button>
+              <div className="dropdown-son-save">
+                <h4>Guardar como imagen</h4>
+                <button>Ordenador</button>
+                <button>PhotoEditor</button>
+                <button>Google Drive</button>
+                <button>Google Fotos</button>
+              </div>
+
+              <div className="dropdown-son-save">
+                <h4>Proyeccto existente</h4>
+                <button>Ordenador</button>
+                <button>PhotoEditor</button>
+              </div>
+            </div>
+
             <div>
-              <button>Ordenador</button>
-              <button>PhotoEditor</button>
+              <button>Batch</button>
             </div>
           </div>
 
           <div>
-            <button>Save <FontAwesomeIcon icon={['fas', 'chevron-down']} /></button>
-            <div>
-
-            </div>
+            <button>
+              <FontAwesomeIcon icon={["fas", "circle-info"]} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={["fas", "circle-user"]} />
+            </button>
           </div>
-
-          <div>
-            <button>Batch</button>
-          </div>
-
-          <div>
-            <button><FontAwesomeIcon icon={['fas', 'circle-info']} /></button>
-            <button><FontAwesomeIcon icon={['fas', 'circle-user']} /></button>
-          </div>
-
         </nav>
-
       </header>
     </>
   );
