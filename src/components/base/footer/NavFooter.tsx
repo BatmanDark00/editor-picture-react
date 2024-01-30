ba345b5cebf7e0b29c06fc21a8a1dc729e3d8ede
Lennwmedia <lennwmedia@gmail.com>
2024-01-29 18:42:05 -0600

    Ajuste del Footer

--- src/components/base/footer/NavFooter.tsx
+++ src/components/base/footer/NavFooter.tsx
@@ -8,25 +8,25 @@ export default function NavFooter() {
         <>
         <footer id='footer'>
             <div>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "layer-group"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "clone"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "border-all"]} /></button>
             </div>
 
             <div>
+                <button><FontAwesomeIcon icon={["fas", "expand"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "compress"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "minus"]} /></button>
                 <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "plus"]} /></button>
                 <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
             </div>
             
             <div>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
-                <button><FontAwesomeIcon icon={["fas", "bars"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "rotate"]} /></button>
+                <button><FontAwesomeIcon icon={["fas", "arrow-turn-up"]} rotation={270}/></button>
+                <button><FontAwesomeIcon icon={["fas", "arrow-turn-down"]} rotation={270} /></button> 
+                <button><FontAwesomeIcon icon={["fas", "clock-rotate-left"]} /></button>
             </div>
         </footer>
         </>
