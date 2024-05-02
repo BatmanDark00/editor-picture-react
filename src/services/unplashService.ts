bfec3b3483c15750af27c68f0a63c5efaa6a1dd1
Francisco Picado <franciscopicado19@gmail.com>
2024-05-02 17:40:37 +0200

    ♻️ refactor service unplash

--- src/services/unplashService.ts
+++ src/services/unplashService.ts
@@ -1,16 +1,10 @@
 import { createApi } from 'unsplash-js';
-import * as nodeFetch from 'node-fetch'
-
-
 
 const accessKey ="rhU-MQW7-BUMW1Mfp-whWdXjkGmuisyD9IQwS8IUSjo";
 
 const unplashService = createApi({  
-  accessKey: accessKey,
- // fetch: nodeFetch.default as unknown as typeof fetch,
+  accessKey: accessKey, 
 });
-
-
 
 
 export default unplashService;
