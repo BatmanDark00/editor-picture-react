dac67de12e35a8fa3b45f77eb0a4ab833ef3659f
Francisco Picado <franciscopicado19@gmail.com>
2024-05-10 16:34:27 +0200

    📱 add responsive cropper

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import { RouteObject } from "react-router-dom";
 
 
+import GridView from '@/views/GridView.tsx';
 import HomeView from '@/views/HomeView.tsx';
 import PictureView from '@/views/PictureView.tsx';
+
 
 
 const routes: RouteObject[] = [
@@ -14,6 +16,10 @@ const routes: RouteObject[] = [
     {
       path: "/editor-picture",
       Component: PictureView,
+    }, 
+    {
+      path: "/editor-picture/grid-view",
+      Component: GridView,
     }, 
   
   ];
---
+++ src/views/GridView.tsx
@@ -0,0 +1,71 @@
+
+
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import "@/assets/scss/views/gridView.scss";
+
+import CropperPicture from "@/components/cropper/CropperPicture";
+import MenuFooter from "@/components/picture/MenuFooter";
+import MenuHeader from "@/components/picture/MenuHeader";
+import MenuLateral from "@/components/picture/MenuLateral";
+import UploadFile from "@/components/common/UploadFile";
+
+import { setUrlImage } from "@/redux/imageCropperSlice";
+import { RootState } from "@/redux";
+
+
+export default function GridView() {
+
+  const [downloadResult, setDownloadResult] = useState<boolean>(false);
+
+  const imageCropper = useSelector((state: RootState) => state.imageCropper); // Utilizando RootState para tipar el estado global
+  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (downloadResult) {
+      setDownloadResult(false);
+    }
+  }, [downloadResult]);
+
+  const handleFileUpload = (file: File) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.result) {      
+        dispatch(setUrlImage(reader.result as string));
+      }
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const downloadImage = () => {
+    setDownloadResult(true);
+  };
+
+  return (
+    <div className="grid-view">
+      <div className="header">  Menu header</div>
+      <div className="menu-lateral">Menu lateral</div>
+      <div className="menu-lateral-secondary">Menu lateral secondary</div>
+      <div className="editor-cropper">  {!imageCropper.urlImage && (
+                <div className="upload-image">                 
+                  <UploadFile onFileUpload={handleFileUpload} />
+                </div>
+              )}
+
+              {imageCropper.urlImage && (
+                <div className="cropper-picture">
+                  <CropperPicture                  
+                    downloadResult={downloadResult}
+                  />
+                </div>
+              )}</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+
