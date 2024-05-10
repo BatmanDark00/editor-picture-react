b153d876e4b1b5fa4d94dc06532a27eaca2f6261
Lennwmedia <lennwmedia@gmail.com>
2024-05-10 17:08:42 -0600

    🎨 add styles on asidebar

--- src/components/picture/MenuLateral.tsx
+++ src/components/picture/MenuLateral.tsx
@@ -30,34 +30,44 @@ export default function MenuLateral() {
     }
   };
 
+  const handleClickInside = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <>
       <div className="clear-fixed"></div>
       <div className="menu-lateral">
-      <section className="section-actions">
-        <aside>
-          {menuLateralData.map((menu, index) => (
-            <div
-              className="item-menu"
-              key={index}
-              onClick={() => handleComponent(menu.component)}
-            >
-              <button>
-                <FontAwesomeIcon icon={menu.icon as IconProp} />
-              </button>
-              <p>{menu.text} </p>
+        <section className="section-actions">
+          <aside>
+            {menuLateralData.map((menu, index) => (
+              <ul
+                className="item-menu"
+                key={index}
+                onClick={() => handleComponent(menu.component)}
+              >
+                <li className="item-menu-li">
+                  <FontAwesomeIcon icon={menu.icon as IconProp} />
+                  <a 
+                   href="#"
+                   className="anchor"
+                  >
+                    {menu.text}
+                  </a>
+                </li>
+              </ul>
+            ))}
+
+            <div className="clear-fixed"></div>
+          </aside>
+        </section>
+        <section className="section-components">
+          {component && (
+            <div className="tool-menu" onClick={handleClickInside}>
+              {renderComponent()}
             </div>
-          ))}
-
-          <div className="clear-fixed"></div>
-        </aside>
-
-       
-      </section>
-      <section className="section-components">
-      {component && <div className="tool-menu">{renderComponent()}</div>}
-
-      </section>
+          )}
+        </section>
       </div>
     </>
   );
