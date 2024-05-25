4d793b5fe11f41750e675135d2bb37fd4bcdbaac
Francisco Picado <franciscopicado19@gmail.com>
2024-05-25 21:22:45 +0200

    Refactorizar para cambiar el tema

--- src/components/common/InputBase.tsx
+++ src/components/common/InputBase.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import "@/assets/styles/components/common/inputBase.scss";
 
 //  Posibles Props a utilizar
 interface InputProps {
   children: React.ReactNode;
   onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   onClick?: () => void;
   value?: string;
 }
 
-function InputBase({ onChange, value, children }: InputProps) {
+function InputBase({ onChange, onKeyDown, value, children }: InputProps) {
   return (
     <>
       <div className="form" >
-        <input type="text" onChange={onChange} value={value} required />
+        <input type="text" onChange={onChange} onKeyDown={onKeyDown} value={value}  />
         <label className="label">
           <span className="text-name">{children}</span>
         </label>
